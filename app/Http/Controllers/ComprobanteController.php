bdca310f76fcfaff50d811a803b5922c21906ac9
jesus <jesustissera@gmail.com>
2021-04-12 17:56:55 -0300

    Implemento configuración para que funcione el generador de pdf.

--- app/Http/Controllers/ComprobanteController.php
+++ app/Http/Controllers/ComprobanteController.php
@@ -9,11 +9,6 @@ use PDF;
 
 class ComprobanteController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
         $comprobanteItem = DB::table('ComprobanteFacturacionItem')->get();
@@ -26,73 +21,8 @@ class ComprobanteController extends Controller
 
     public function descargarComprobante(){
         $comprobanteItem = DB::table('ComprobanteFacturacionItem')->get();
-        $pdf = PDF::loadView('pdf_view', $comprobanteItem);
-        return $pdf->download('pdf_file.pdf');
+        $pdf = PDF::loadView('pdf/comprobante', $comprobanteItem);
+        return $pdf->download('name.pdf');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Comprobante  $comprobante
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Comprobante $comprobante)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Comprobante  $comprobante
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Comprobante $comprobante)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Comprobante  $comprobante
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Comprobante $comprobante)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Comprobante  $comprobante
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Comprobante $comprobante)
-    {
-        //
-    }
 }
